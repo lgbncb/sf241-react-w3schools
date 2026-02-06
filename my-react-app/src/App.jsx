89aa63151dcbe438bc28b7cbbdc4830225fe643c
Lance B. <lmbuncab@student.apc.edu.ph>
2026-02-06 06:35:00 +0000

    Task 02 update your Vite-React website with sample code from W3Schools

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World, and Hello Philippines! This is your Host, Lance Buncab.</h1>
+      <h2> Pinoy Big Brother</h2>
+    </div>
+  );
+}
+
+export default App;
