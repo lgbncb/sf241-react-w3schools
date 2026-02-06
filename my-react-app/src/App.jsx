ce76aea53bdf000824cf64911afc8066f4c7ebe1
Lance B. <lmbuncab@student.apc.edu.ph>
2026-02-06 14:55:56 +0800

    Add hosting information to App component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     <div className="App">
       <h1>Hello World, and Hello Philippines! This is your Host, Lance Buncab.</h1>
       <h2> Pinoy Big Brother</h2>
+      <h3> Hosted in Vercel.com</h3>
     </div>
   );
 }
